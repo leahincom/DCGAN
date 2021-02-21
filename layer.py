7a69c7df1d371429e7e40ff8973ab275909f4dd9
leahincom <leahincom@gmail.com>
2021-02-21 23:05:11 +0000

    update dataset.py util.py

--- layer.py
+++ layer.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class DECBR2d(nn.Module):
+class DECBR2d(nn.Module):  # generator
     def __init__(self, in_channels, out_channels, kernel_size=3,
                  stride=1, padding=1, bias=True, norm="bnorm", relu=0.0):
         super().__init__()
@@ -15,13 +15,36 @@ class DECBR2d(nn.Module):
 
         if not norm is None:
             if norm == 'bnorm':
-                # generator
                 layers += [nn.BatchNorm2d(num_features=out_channels)]
             else norm == 'inorm':
                 layers += [nn.InstanceNorm2d(num_features=out_channels)]
 
         if not relu is None and relu >= 0.0:
             layers += [nn.ReLU() if relu == 0 else nn.LeakyReLU(relu)]
+
+        self.cbr = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.cbr(x)
+
+
+class CBR2d(nn.Module):  # discriminator
+    def __init__(self, in_channels, out_channels, kernel_size=3,
+                 stride=1, padding=1, bias=True, norm='bnorm', relu=0.0):
+        super().__init__()
+
+        layer = []
+        layer += nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
+                           kernel_size=kernel_size, stride=stride, padding=padding, bias=bias)
+
+        if not norm is None:
+            if norm == 'bnorm':
+                layers += [nn.BatchNorm2d(num_features=out_channels)]
+            elif norm == 'inorm':
+                layers += [nn.InstanceNorm2d(num_features=out_channels)]
+
+        if not relu is None and relu >= 0.0:
+            layers += [nn.ReLU() if relu == 0.0 else nn.LeakyReLU(relu)]
 
         self.cbr = nn.Sequential(*layers)
 
--- model.py
+++ model.py
@@ -5,6 +5,15 @@ import torch.nn as nn
 from torch.nn import init
 from torch.optim import lr_schedular
 
+# This network takes in a 100x1 noise vector, denoted z,
+# and maps it into the G(Z) output which is 64x64x3.
+# The first layer expands the random noise from 100x1 to 1024x4x4!
+# This layer is denoted as ‘project and reshape’.
+# After this, a fractionally-strided convolution is applied 4 times
+# with stride of 2  and kernel filter 5x5.
+# (when convolution applied, it doubles the image dimension
+# while reducing the number of output channels in each phase).
+
 
 class Generator(nn.Module):
     def __init__(self, in_channels, out_channels, nker=128, norm='bnorm'):
@@ -12,13 +21,13 @@ class Generator(nn.Module):
 
         self.dec1 = DECBR2d(1*in_channels, 8*nker, kernel_size=4, stride=1,
                             padding=0, norm=norm, relu=0.0, bias=False)
-        self.dec2 = DECBR2d(8*in_channels, 4*nker, kernel_size=4, stride=2,
+        self.dec2 = DECBR2d(8*nker, 4*nker, kernel_size=4, stride=2,
                             padding=0, norm=norm, relu=0.0, bias=False)
-        self.dec3 = DECBR2d(4*in_channels, 2*nker, kernel_size=4, stride=2,
+        self.dec3 = DECBR2d(4*nker, 2*nker, kernel_size=4, stride=2,
                             padding=0, norm=norm, relu=0.0, bias=False)
-        self.dec4 = DECBR2d(2*in_channels, 1*nker, kernel_size=4, stride=2,
+        self.dec4 = DECBR2d(2*nker, 1*nker, kernel_size=4, stride=2,
                             padding=0, norm=norm, relu=0.0, bias=False)
-        self.dec5 = DECBR2d(1*in_channels, out_channels, kernel_size=4, stride=2,
+        self.dec5 = DECBR2d(1*nker, out_channels, kernel_size=4, stride=2,
                             padding=0, norm=norm, relu=0.0, bias=False)
 
     def forward(self, x):
@@ -28,5 +37,31 @@ class Generator(nn.Module):
         x = self.dec4(x)
         x = self.dec5(x)
         x = torch.tanh(x)
+
+        return x
+
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels, out_channels, nker=64, norm='bnorm'):
+        super(Discriminator, self).__init__()
+
+        self.enc1 = CBR2d(1*in_channels, 1*nker, kernel_size=4, stride=2,
+                          padding=1, norm=norm, relu=0.2, bias=False)
+        self.enc2 = CBR2d(1*nker, 2*nker, kernel_size=4, stride=2,
+                          padding=1, norm=norm, relu=0.2, bias=False)
+        self.enc3 = CBR2d(2*nker, 4*nker, kernel_size=4, stride=2,
+                          padding=1, norm=norm, relu=0.2, bias=False)
+        self.enc4 = CBR2d(4*nker, 8*nker, kernel_size=4, stride=2,
+                          padding=1, norm=norm, relu=0.2, bias=False)
+        self.enc5 = CBR2d(8*nker, out_channels, kernel_size=4, stride=2,
+                          padding=1, norm=norm, relu=0.2, bias=False)
+
+    def forward(self, x):
+        x = self.enc1(x)
+        x = self.enc2(x)
+        x = self.enc3(x)
+        x = self.enc4(x)
+        x = self.enc5(x)
+        x = torch.sigmoid(x)
 
         return x
